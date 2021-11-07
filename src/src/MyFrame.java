5ca953a596d815295d9018a573d441183d580c63
jacek010 <jmyjkowski@gmail.com>
2021-11-07 14:46:56 +0100

    Zrobiono przyciski i prostego action listenera

--- src/src/MyFrame.java
+++ src/src/MyFrame.java
@@ -1,14 +1,25 @@
 package src;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class MyFrame extends JFrame {
 
     public MyFrame(){
         super("Nazwa okienka");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        pack();
+        setSize(300, 100);
+        setLocation(500, 500);
         setResizable(true);
+        setLayout(new FlowLayout());
+
+        add(new JButton("Button 1"));
+        add(new JButton("Button 2"));
+        add(new JButton("Button 3"));
+
+        JPanel buttonPanel = new Przycisk();
+        add(buttonPanel);
+        pack();
         setVisible(true);
     }
 }
